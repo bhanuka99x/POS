276cd10c619645626090eb32b2b3f18e0472df4b
Bhanuka Gihan <51521654+Bhanuka99x@users.noreply.github.com>
2025-04-18 18:53:38 +0530

    design login

--- src/main/java/com/posapp/controllers/LoginScreenController.java
+++ src/main/java/com/posapp/controllers/LoginScreenController.java
@@ -1,4 +1,19 @@
 package com.posapp.controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginScreenController {
+
+    @FXML
+    private Button btnlogin;
+
+    @FXML
+    private PasswordField txtpassword;
+
+    @FXML
+    private TextField txtuser;
+
+
 }
