6e59aaa83865e713f79cb534bb57aad2a23ac7eb
Bhanuka Gihan <51521654+Bhanuka99x@users.noreply.github.com>
2025-05-05 00:25:31 +0530

    inventory ui

---
+++ src/main/java/com/posapp/controllers/InventoryManagementController.java
@@ -0,0 +1,132 @@
+package com.posapp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class InventoryManagementController {
+
+    @FXML
+    private Button btnadd;
+
+    @FXML
+    private Button btncustomers;
+
+    @FXML
+    private Button btndelete;
+
+    @FXML
+    private Button btninventory;
+
+    @FXML
+    private Button btnlogout;
+
+    @FXML
+    private Button btnmenu;
+
+    @FXML
+    private Button btnope;
+
+    @FXML
+    private Button btnorders;
+
+    @FXML
+    private Button btnreports;
+
+    @FXML
+    private Button btnupdate;
+
+    @FXML
+    private Button btnupload;
+
+    @FXML
+    private TableColumn<?, ?> imagecell;
+
+    @FXML
+    private ImageView imageview;
+
+    @FXML
+    private TableColumn<?, ?> namecell;
+
+    @FXML
+    private TableColumn<?, ?> pricecell;
+
+    @FXML
+    private TableColumn<?, ?> quantitycell;
+
+    @FXML
+    private TableView<?> tblinventory;
+
+    @FXML
+    private Label txtlbl;
+
+    @FXML
+    private TextField txtname;
+
+    @FXML
+    private TextField txtprice;
+
+    @FXML
+    private TextField txtquantity;
+
+    @FXML
+    void clickadd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickcustomers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickdelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickinventory(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clicklogout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickmenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickope(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickorders(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickreports(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickupdate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickupload(ActionEvent event) {
+
+    }
+
+}
